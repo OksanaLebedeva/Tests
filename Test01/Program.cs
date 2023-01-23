c8a08874bdaf57532e29b61268fe6a1a804d348c
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-23 20:51:41 +0300

    Added solution

---
+++ Test01/Program.cs
@@ -0,0 +1,31 @@
+﻿void PrintArray(string[] array)
+{
+  for (int i=0; i<array.Length; i++)
+  {
+    Console.Write($"'{array[i]}'");
+    if (i<array.Length-1) Console.Write(", ");
+    else Console.Write("");
+  }
+  Console.WriteLine("]");
+}
+
+string[] initialArray = {"computer", "science", "123", "3", "million", "A"}; 
+Console.Write($"Initial array: [");
+PrintArray(initialArray);
+int newArraySize = 0;
+for (int i=0; i<initialArray.Length; i++)
+{
+  if (initialArray[i].Length <= 3) newArraySize++;
+}
+string[] newArray = new string [newArraySize];
+newArraySize = 0;
+for (int i=0; i<initialArray.Length; i++)
+{
+  if (initialArray[i].Length <= 3)
+  {
+    newArray[newArraySize] = initialArray[i];
+    newArraySize++;
+  }
+}
+Console.Write($"Final array: [");
+PrintArray(newArray);
